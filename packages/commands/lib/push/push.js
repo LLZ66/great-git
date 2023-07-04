1f85dadbee22436ac4b3338b1a4a181994b8ea32
LLZ66 <1228429428@qq.com>
2023-07-04 13:58:48 +0800

    fix(修复push提示):修复push提示

--- packages/commands/lib/push/push.js
+++ packages/commands/lib/push/push.js
@@ -8,9 +8,8 @@ async function Init(force) {
     const spinner = ora('正在提交代码...').start();
     const result = await execa(PUSH_COMMAND(force));
     if(!result.failed) {
-        log.info('push成功');
-        spinner.clear();
         spinner.stop();
+        log.info('push成功');
         return true
     }else {
         printErrorLog(stderr);
