cbd847141bba6ba3a7a55c9b82ea198c5a0970a7
LLZ66 <1228429428@qq.com>
2023-07-04 14:06:46 +0800

    test(*):测试push

--- packages/commands/lib/push/push.js
+++ packages/commands/lib/push/push.js
@@ -4,17 +4,21 @@ import { log } from '@llzcli/utils'
 
 const PUSH_COMMAND = (force) => `git push ${force? '-f':''}`
 
+function handlePushError(err) {
+    console.log(err);
+}
+
 async function Init(force) {
     const spinner = ora('正在提交代码...').start();
-    const result = await execa(PUSH_COMMAND(force));
-    if(!result.failed) {
+    try {
+        await execa(PUSH_COMMAND(force));
         spinner.stop();
         log.info('push成功');
-        return true
-    }else {
-        printErrorLog(stderr);
-        spinner.stop();
-        throw new Error(`执行git push失败: 原因是:${stderr}`)
+    }catch(err) {
+        handlePushError(err)
+        // printErrorLog(err);
+        // spinner.stop();
+        // throw new Error(`执行git push失败: 原因是:${err}`)
     }
 };
 
