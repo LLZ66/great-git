ef012708463b694b4396b5bc7712d2326a5fe3de
LLZ66 <1228429428@qq.com>
2023-07-12 15:33:33 +0800

    fix(*):33

--- packages/commands/lib/push/push.js
+++ packages/commands/lib/push/push.js
@@ -19,40 +19,48 @@ function isConflct(err) {
     return err.indexOf("Automatic merge failed; fix conflicts and then commit the result.") !== -1
 }
 
+async function handleRePull() {
+    await runCommand({
+        command: PULL_COMMAND,
+        loading: '正在拉取代码...',
+        successMsg: '拉取成功,无冲突',
+        errorCb: handlePushError
+    });
+    const needRepush = await makeConfirm({
+        message: `是否需要继续提交`
+    });
+    if(needRepush) {
+        await Init();
+    }
+}
+
+async function handleConflct() {
+    log.info('当前提交有冲突,请手动合并冲突后再次提交1');
+    // modification completed
+    const isMc = await makeConfirm({
+        message: `是否修改冲突完成`
+    });
+    if(isMc) {
+        await doAdd(false);
+        await doCommit({
+            type: 'merge'
+        });
+        await Init();
+    }
+}
+
 async function handlePushError(err) {
-    console.log(err.stderr);
+    console.log(err, 'err');
     if(isNeedPull(err.stderr)) {
         const needPull = await makeConfirm({
             message: `当前提交分支有更改,是否需要下拉最新代码(如果需要手动修改,请终止流程后手动修改)`
         });
         if(needPull) {
-            await runCommand({
-                command: PULL_COMMAND,
-                loading: '正在拉取代码...',
-                successMsg: '拉取成功,无冲突',
-                errorCb: handlePushError
-            });
-            const needRepush = await makeConfirm({
-                message: `是否需要继续提交`
-            });
-            if(needRepush) {
-                await Init();
-            }
+            await handleRePull()
         }
     }
     if(isConflct(err.stdout)) {
-        log.info('当前提交有冲突,请手动合并冲突后再次提交');
-        // modification completed
-        const isMc = await makeConfirm({
-            message: `是否修改冲突完成`
-        });
-        if(isMc) {
-            await doAdd(false);
-            await doCommit({
-                type: 'merge'
-            });
-            await Init();
-        }
+        await handleConflct()
     };
     return true;
 }
